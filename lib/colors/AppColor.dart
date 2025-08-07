89313763da4dadec0d8794dfdc2ed3f0bfd700d5
Andrew417 <andrewehab417@gmail.com>
2025-08-08 01:24:40 +0300

    finished the project

--- lib/colors/AppColor.dart
+++ lib/colors/AppColor.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color Appbar = Color(0xff265ed7);
   static const Color ChatButton = Color(0xff4cb050);
+  static const Color whiteContainer = Color(0xffe4f2fd);
 }
--- lib/pages/MainPage.dart
+++ lib/pages/MainPage.dart
@@ -29,153 +29,349 @@ class _MainPageState extends State<MainPage> {
         backgroundColor: AppColors.Appbar,
         titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            // Profile Section
-            Expanded(
-              flex: 2,
-              child: Row(
-                children: [
-                  // Profile Image
-                  Expanded(
-                    flex: 1,
-                    child: CircleAvatar(
-                      radius: 40,
-                      backgroundImage: AssetImage(
-                        'assets/profile.jpg',
-                      ), // replace with your image
-                    ),
-                  ),
-                  const SizedBox(width: 12),
 
-                  // Profile Info
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text(
-                          'Dr. Sayed Ali',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 4),
-                        Text('Eye Special'),
-                        SizedBox(height: 4),
-                      ],
-                    ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              // Profile Section
+              SizedBox(height: 15),
+              profile(),
+
+              // ABout Section
+              SizedBox(height: 20),
+              AboutSection(),
+              SizedBox(height: 20),
+
+              const Divider(color: Colors.black, thickness: 1.5),
+              const SizedBox(height: 20),
+
+              // COntact Info
+              ContactInfo(),
+
+              const SizedBox(height: 15),
+
+              // chat button
+              chatBtn(),
+              const SizedBox(height: 12),
+
+              // bookBtn
+              bookBtn(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget profile() {
+    return Row(
+      children: [
+        // Profile Image
+        Expanded(
+          child: ClipOval(
+            child: Image.network(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpfNhYMBE1ZFsPhaD4VHz1NdVCC9j2WU7uHQ&s',
+              height: 150,
+              width: 100,
+            ),
+          ),
+        ),
+
+        SizedBox(width: 12),
+
+        // Profile Info
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Dr. Sayed Abdul-Aziz',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.Appbar,
+                ),
+              ),
+              const Text('Eye Special'),
+              const SizedBox(height: 8),
+              const Row(
+                children: [
+                  Icon(Icons.star, color: Colors.amber, size: 20),
+                  SizedBox(width: 4),
+                  Text(
+                    '4.8',
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
                   ),
                 ],
               ),
-            ),
-
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text(
-                  'About',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 12),
-                Text(
-                  'Professor of Eye Special - Former Head of Department of Eye Special, Cairo University',
-                  style: TextStyle(fontSize: 14),
-                ),
-                SizedBox(height: 12),
-              ],
-            ),
-
-            // About
-            Expanded(
-              child: Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(12),
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: const Text('', style: TextStyle(fontSize: 14)),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.call, size: 20),
+                    style: IconButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                      backgroundColor: AppColors.whiteContainer,
+                    ),
+                    onPressed: () {},
+                  ),
+                  const SizedBox(width: 8),
+                  IconButton(
+                    icon: Icon(Icons.call, size: 20),
+                    style: IconButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                      backgroundColor: AppColors.whiteContainer,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
               ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget AboutSection() {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'About',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 12),
+          Text(
+            'Professor of Eye Special - Former Head of Department of Eye Special, Cairo University',
+            style: TextStyle(fontSize: 14),
+          ),
+          SizedBox(height: 12),
+
+          Container(
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: AppColors.whiteContainer,
+              borderRadius: BorderRadius.circular(8),
             ),
-
-            const SizedBox(height: 12),
-
-            // Contact Info
-            Expanded(
-              child: Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(12),
-                decoration: BoxDecoration(
-                  color: Colors.grey[100],
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Text('dr@6.com'),
-                    SizedBox(height: 4),
-                    Text('01032564823'),
-                    SizedBox(height: 4),
-                    Text('01125466125'),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 30,
+                      height: 30,
+                      decoration: const BoxDecoration(
+                        color: AppColors.Appbar,
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Icon(
+                        Icons.local_hospital,
+                        size: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    const Text('Cleopatra Hospital'),
                   ],
                 ),
-              ),
+                const SizedBox(height: 12),
+
+                Row(
+                  children: [
+                    Container(
+                      width: 30,
+                      height: 30,
+                      decoration: const BoxDecoration(
+                        color: AppColors.Appbar,
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Icon(
+                        Icons.access_time,
+                        size: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    const Text('10 - 19'),
+                  ],
+                ),
+                const SizedBox(height: 12),
+
+                Row(
+                  children: [
+                    Container(
+                      width: 30,
+                      height: 30,
+                      decoration: const BoxDecoration(
+                        color: AppColors.Appbar,
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Icon(
+                        Icons.location_on,
+                        size: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    const Text('2 Gamma Street, Giza'),
+                  ],
+                ),
+              ],
             ),
+          ),
+        ],
+      ),
+    );
+  }
 
-            const SizedBox(height: 12),
+  Widget ContactInfo() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Contact Info',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 12),
 
-            // contact info
-            SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColors.ChatButton,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+        Container(
+          padding: const EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: AppColors.whiteContainer,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 30,
+                    height: 30,
+                    decoration: const BoxDecoration(
+                      color: AppColors.Appbar,
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Icon(
+                      Icons.email,
+                      size: 16,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                onPressed: () {},
-                child: const Text(
-                  'Chat With Dr. Sayed',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                  ),
-                ),
+                  const SizedBox(width: 8),
+                  const Text('dr@6.com'),
+                ],
               ),
-            ),
-            const SizedBox(height: 12),
+              const SizedBox(height: 12),
 
-            // --- Book Button ---
-            SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColors.Appbar,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+              Row(
+                children: [
+                  Container(
+                    width: 30,
+                    height: 30,
+                    decoration: const BoxDecoration(
+                      color: AppColors.Appbar,
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Icon(
+                      Icons.call,
+                      size: 16,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                onPressed: () {},
-                child: const Text(
-                  'Book an Appointment',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
+                  const SizedBox(width: 8),
+                  const Text('01032564823'),
+                ],
+              ),
+              const SizedBox(height: 12),
+
+              Row(
+                children: [
+                  Container(
+                    width: 30,
+                    height: 30,
+                    decoration: const BoxDecoration(
+                      color: AppColors.Appbar,
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Icon(
+                      Icons.call,
+                      size: 16,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
+                  const SizedBox(width: 8),
+                  const Text('01125466125'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  SizedBox chatBtn() {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.ChatButton,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+
+        onPressed: () {},
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.message, color: Colors.white),
+            SizedBox(width: 10),
+            Text(
+              'Chat With Dr. Sayed',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Colors.white,
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  SizedBox bookBtn() {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.Appbar,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        onPressed: () {},
+        child: const Text(
+          'Book an Appointment',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            color: Colors.white,
+          ),
         ),
       ),
     );
